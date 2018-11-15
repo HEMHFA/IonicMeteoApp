023430650dfd880c5c7bb99f906e456ba5e4a0c5
HEMHFA <arnaud.hugo@orange.fr>
2018-11-15 16:33:00 +0100

    interface CityInfo + données Json + provider meteo-api

---
+++ interface/CityInfo.ts
@@ -0,0 +1,9 @@
+export interface CityInfo {
+    name: string;
+    country: string;
+    latitude: string;
+    longitude: string;
+    elevation: string;
+    sunrise: string;
+    sunset: string;
+}
---
+++ src/providers/meteo-api/meteo-api.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Platform } from "ionic-angular";
+import { Observable } from "rxjs/Rx";
+import { CityInfo } from "../../../interface/CityInfo";
+
+/*
+  Generated class for the MeteoApiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MeteoApiProvider {
+  private baseUrl: string = "../../assets/api/movies.json";
+
+  constructor(private readonly http: HttpClient,
+    private readonly platform: Platform) {
+    console.log('Hello MeteoApiProvider Provider');
+  }
+
+  getAllDays(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+}
